c3197897790f886df4170a9d8f53b2d650681db7
gl0balscope <pilks@tuta.io>
2022-02-16 20:31:24 -0500

    init

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  generatorStarSpacing: true,
+  spaceBeforeFunctionParen: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: false,
+  yieldStarSpacing: true,
+  alignTernaryLines: false,
+  trailingComma: 'none',
+  printWidth: 120
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Sean Javascript
+
+Basic, minimum setup for javascript projects.
